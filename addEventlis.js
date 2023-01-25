14191e5278894c734d2639ee4772a25621e6ba3e
Sachin Thakkar <112240368+srt-1909@users.noreply.github.com>
2023-01-25 10:43:12 +0530

    Add files via upload

---
+++ addEventlis.js
@@ -0,0 +1,15 @@
+document.getElementById("img1").addEventListener("mouseover", function () {
+  this.style.filter = "grayscale(0%)";
+});
+
+document.getElementById("img1").addEventListener("mouseout", function () {
+  this.style.filter = "grayscale(100%)";
+});
+
+document.getElementById("img2").addEventListener("mouseover", function () {
+    this.style.filter = "grayscale(0%)";
+  });
+  
+  document.getElementById("img2").addEventListener("mouseout", function () {
+    this.style.filter = "grayscale(100%)";
+  });
